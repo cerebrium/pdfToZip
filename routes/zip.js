a439475da6783b478b8dcae6b8ff2f4c69afaea1
cerebrium <nicholas.m.shankland@gmail.com>
2020-10-18 16:57:05 -0700

    fixed

--- routes/zip.js
+++ routes/zip.js
@@ -4,25 +4,6 @@ var JSZip = require("jszip");
 var fetch = require("node-fetch");
 var fs = require('fs');
 var path = require('path');
-  
-  let namesArray = [
-    "ALEKSANDAR TOTEV WEEK 35 INV 6438 DSN1",
-    "BRUNO CROSCATO WEEK 35 INV 6423 DSN1",
-    "BRUNO KURSCHAT WEEK 35 INV 6424 DSN1",
-    "CARLOS CONCEICAO WEEK 35 INV 6425 DSN1",
-    "EVELYN RIBEIRO WEEK 35 INV 6426 DSN1",
-    "FABIO USTULIN WEEK 35 INV 6427 DSN1",
-    "IOLANDA DE LIMA WEEK 35 INV 6428 DSN1",
-    "LEONARDO LUGLI WEEK 35 INV 6429 DSN1",
-    "MARCELO BARBOSA WEEK 35 INV 6430 DSN1",
-    "MARCOS CORDEIRO WEEK 35 INV 6433 DSN1",
-    "MARCUS SIMPLICIO WEEK 35 INV 6431 DSN1",
-    "MARIN KOSTOV WEEK 35 INV 6432 DSN1",
-    "NIKOLAY KEHAYOV WEEK 35 INV 6434 DSN1",
-    "PAULO MARCHESI WEEK 35 INV 6435",
-    "PEDRO JESUS PAULOS WEEK 35 INV 6436 DSN1",
-    "STANIMIR GEORGIEV WEEK 35 INV 6437 DSN1"
-  ]
 
 // basic get route that tells you what to do
 router.get('/', function(req, res) {
@@ -49,7 +30,7 @@ router.post('/', function (req, res) {
                     let getData = async function(urlNum, url) {
                         await fetch(url).then( response => response.blob()).then( response => response.arrayBuffer()).then( response => {
                             console.log('this is the response:', response, '\n', urlNum, '\n', url)
-                            pdfZip.file(`${req.body.names[urlNum]}.pdf`, response, {base64: true})
+                            pdfZip.file(`${nameAttay[urlNum]}.pdf`, response, {base64: true})
                             try {
                                 pdfZip.generateNodeStream({
                                     type:'nodebuffer',
